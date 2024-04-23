847448f8b9178061ad34d2035310a450fae75a1e
Rick Haws <richard.haws@us.af.mil>
2024-04-23 13:44:20 -0600

    initial commit

---
+++ keyboard-workshop-vscode.ts
@@ -0,0 +1,25 @@
+// *** VS Code shortcuts
+
+/**
+ * This tutorial assumes you are already comfortable with common shortcuts
+ * that are available in most programs:
+ *      Shift-Left/Right/Up/Down - Extend or shrink selection right/left/up/down
+ *      Shift-Home/End - Extend selection to beginning/end of current line
+ *      Ctrl-Left/Right - Move cursor one word at a time
+ *      Ctrl-Home/End - Move cursor to top/bottom of file
+ *      Shift-Ctrl-Left/Right - Extend or shrink selection one word at a time
+ *      Shift-Ctrl-Home/End - Extend selection to beginning/end of file
+ *      Ctrl-C/X - Copy/Cut selection to clipboard
+ *      Ctrl-V - Paste from clipboard
+ *      Ctrl-Z/Y - Undo/Redo
+ */
+
+// 1 - Use constantly
+
+// 2 - Use many times per session
+
+// 3 - Use frequently
+
+// 4 - Use occasionally
+
+// 5 - Also available
